7b24919be3245047e0baba2aac615d37d3826254
Mindica <twilightflavor@gmail.com>
2024-10-31 06:56:59 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+//        Zombie zombie1 = new Zombie("Alice");
+//        zombie1.attack();
+//        zombie1.growl();
+//        Zombie zombie2 = new Zombie("Bob");
+//        zombie2.attack();
+//        zombie2.growl();
+        Zombie zombie = new Zombie("Bob");
+        zombie.attack();
+    }
+}
